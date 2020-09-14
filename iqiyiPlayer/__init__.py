b5ba294ee01ffd1a1c3b8acf04e5cbd07f008d9c
whutfaker <1312976124@qq.com>
2020-09-14 15:45:16 +0800

    add

---
+++ iqiyiPlayer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/12 9:39
+# @Author  : AX
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+import requests
+def iqiyi(keyword):
+    pass
+
+
+if __name__ == "__main__":
+    pass
--- txplayer/test.py
+++ txplayer/test.py
@@ -5,5 +5,8 @@
 # @Software: PyCharm
 
 import txplayer
+
 if __name__ == '__main__':
-    print(txplayer.searchTXplayer('武庚纪'))
+    data = txplayer.searchTXplayer('武庚纪')
+    data = txplayer.get_page_message(data[0]['id'], str(data[0]['current_count']))
+    print(data)
